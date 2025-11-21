7268c9a3740656c3db922b8799f77981763ef929
MaiaS2026 <maia.salgado2026@gbwl.org>
2025-11-21 14:41:12 +0000

    made pi

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("hello world")
+import pandas as pd
+from matplotlib import pyplot as plt
 
+# Set style theme
+plt.style.use('seaborn-v0_8-deep')
 
-if __name__ == "__main__":
-    main()
+# Read CSV into DataFrame
+df = pd.read_csv('class-data-v1.csv')
+df = df.head(8) # Limit to first 8 rows for our class
+
+# Some cols need type converted (objecr -> datetime, boolean)
+df['Birthdate'] = pd.to_datetime(df['Birthdate'])
+df['Wakeup Time Weekday'] = pd.to_datetime(df['Wakeup Time Weekday'], format='%H:%M').dt.time
+df['Wakeup Time Weekend'] = pd.to_datetime(df['Wakeup Time Weekend'], format='%H:%M').dt.time
+
+# ZODIAC PIE CHART
+zodiac_counts = df['Zodiac Element'].value_counts()
+colors = ["#df3b3b","#3389df","#5f8e5f","#c4c4c4"]
+plt.pie(zodiac_counts.values, labels=zodiac_counts.index, colors=colors, startangle=90, autopct='%1.1f%%', wedgeprops={'edgecolor': 'black'})
+plt.title('Distribution of Zodiac Elements') 
+plt.axis('equal')
+plt.savefig('zodiac_element_pie.png', bbox_inches='tight')
+plt.close()
